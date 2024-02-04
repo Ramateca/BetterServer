8a31a7e40a7cbe65d72aa1609aa214f0690ab5e7
Ramateca <mataceragiorgio@gmail.com>
2024-02-04 14:45:19 +0100

    init

---
+++ src/main/java/com/wildmagicianshowroom/mc/betterserver/BetterServer.java
@@ -0,0 +1,17 @@
+package com.wildmagicianshowroom.mc.betterserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
